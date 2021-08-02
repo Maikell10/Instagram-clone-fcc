8d6cb2f9ae631661d2c43400091ad14dde1b3f7f
Maikell <maikell.ods10@gmail.com>
2021-08-01 22:01:52 -0400

    Post Feed System

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -1,10 +1,79 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text, Image, FlatList, Button } from "react-native";
 
-export default function Feed() {
+import firebase from "firebase";
+require("firebase/firestore");
+import { connect } from "react-redux";
+
+function Feed(props) {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        let posts = [];
+        if (props.usersLoaded == props.following.length) {
+            for (let i = 0; i < props.following.length; i++) {
+                const user = props.users.find(
+                    (el) => el.uid === props.following[i]
+                );
+                if (user != undefined) {
+                    posts = [...posts, user.posts];
+                }
+            }
+
+            posts.sort(function (x, y) {
+                return x.creation - y.creation;
+            });
+
+            setPosts(posts);
+        }
+    }, [props.usersLoaded]);
+
     return (
-        <View>
-            <Text>Feed</Text>
+        <View style={styles.container}>
+            <View style={styles.containerGallery}>
+                <FlatList
+                    numColumns={3}
+                    horizontal={false}
+                    data={posts}
+                    renderItem={({ item }) => (
+                        <View style={styles.containerImage}>
+                            <Image
+                                style={styles.image}
+                                source={{ uri: item.downloadURL }}
+                            />
+                        </View>
+                    )}
+                />
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    containerInfo: {
+        margin: 20,
+    },
+    containerGallery: {
+        flex: 1,
+    },
+    containerImage: {
+        flex: 1 / 3,
+    },
+    image: {
+        flex: 1,
+        aspectRatio: 1 / 1,
+        height: 120,
+    },
+});
+
+const mapStateToProps = (store) => ({
+    currentUser: store.userState.currentUser,
+    following: store.userState.following,
+    users: store.usersState.users,
+    usersLoaded: store.usersState.usersLoaded,
+});
+
+export default connect(mapStateToProps, null)(Feed);
